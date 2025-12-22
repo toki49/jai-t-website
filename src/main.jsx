fbbabcff802aa8b7a2f42c387a721593ee7a8f4c
Togzhan Zhumadil <togzhanzhumadil@gmail.com>
2025-12-22 18:36:47 -0500

    removed mention of /jai-t-website from the router for cloudfare

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,7 @@ import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  
-<BrowserRouter basename="/jai-t-website">
-  <App />
-</BrowserRouter>,
-)
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
